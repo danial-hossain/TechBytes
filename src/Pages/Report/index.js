b7172af0ee8b52abcd37227f020ca2bdd996955b
Mahek <mahektasnia@gmail.com>
2025-09-14 11:37:40 +0600

    Hello

--- src/Pages/Report/index.js
+++ src/Pages/Report/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
-
+//hello
 const ReportPage = () => {
   const [opinion, setOpinion] = useState("");
   const [message, setMessage] = useState("");
